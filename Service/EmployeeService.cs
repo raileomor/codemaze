bd4d0f9b3431ec80853e70891e1061bcbb598023
Rainer Leon <raileomor@gmail.com>
2021-12-02 00:09:41 -0300

    Parent/Child relationships

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -1,6 +1,9 @@
 using AutoMapper;
 using Contracts;
+using Entities.Exceptions;
+using Entities.Models;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 
 namespace Service;
 
@@ -15,5 +18,31 @@ internal sealed class EmployeeService : IEmployeeService
         _repository = repository;
         _logger = logger;
         _mapper = mapper;
+    }
+
+    public IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges)
+    {
+        var company = _repository.Company.GetCompany(companyId, trackChanges);
+        if (company is null)
+            throw new CompanyNotFoundException(companyId);
+
+        var employeesFromDb = _repository.Employee.GetEmployees(companyId, trackChanges);
+        var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
+
+        return employeesDto;
+    }
+
+    public EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges)
+    {
+        var company = _repository.Company.GetCompany(companyId, trackChanges);
+        if (company is null)
+            throw new CompanyNotFoundException(companyId);
+
+        var employeeDb = _repository.Employee.GetEmployee(companyId, id, trackChanges);
+        if (employeeDb is null)
+            throw new EmployeeNotFoundException(id);
+
+        var employee = _mapper.Map<EmployeeDto>(employeeDb);
+        return employee;
     }
 }
