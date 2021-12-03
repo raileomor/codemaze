d70e5e6ab1c92a3331db49d05a6026ca5845211f
Rainer Leon <raileomor@gmail.com>
2021-12-02 23:11:19 -0300

    Content Negotiation

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -33,4 +33,8 @@ public static class ServiceExtensions {
         IConfiguration configuration) =>
             services.AddDbContext<RepositoryContext>(opts =>
                 opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+    public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
+        builder.AddMvcOptions(config =>
+            config.OutputFormatters.Add(new CsvOutputFormatter()));
 }
