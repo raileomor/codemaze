b9672eed330bd49add25ceece5db2bf78f270aa6
Rainer Leon <raileomor@gmail.com>
2021-12-04 23:03:04 -0300

    Working with DELETE requests

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -22,4 +22,6 @@ public class EmployeeRepository: RepositoryBase<Employee>, IEmployeeRepository
         employee.CompanyId = companyId;
         Create(employee);
     }
+
+    public void DeleteEmployee(Employee employee) => Delete(employee);
 }
