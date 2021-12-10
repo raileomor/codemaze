1a8e5313f715547d6ce25394580336cdc3fd056c
Rainer Leon <raileomor@gmail.com>
2021-12-09 22:50:42 -0300

    Filtering

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -24,6 +24,9 @@ internal sealed class EmployeeService : IEmployeeService
     public async Task<(IEnumerable<EmployeeDto> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId,
         EmployeeParameters employeeParameters, bool trackChanges)
     {
+        if (!employeeParameters.ValidAgeRange)
+            throw new MaxAgeRangeBadRequestException();
+
         await CheckIfCompanyExists(companyId, trackChanges);
 
         var employeesWithMetaData = await _repository.Employee
