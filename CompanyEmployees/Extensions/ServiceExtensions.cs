ff3e72d24026712e54ca6e9fa05d213332c2bc9b
Rainer Leon <raileomor@gmail.com>
2022-01-23 14:55:58 -0300

    Rate Limiting and Throttling

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc.Versioning;
 using CompanyEmployees.Presentation;
 using CompanyEmployees.Presentation.Controllers;
 using Marvin.Cache.Headers;
+using AspNetCoreRateLimit;
 
 namespace CompanyEmployees.Extensions;
 
@@ -103,4 +104,23 @@ public static class ServiceExtensions {
 		{
 			validationOpt.MustRevalidate = true;
 		});
+
+    public static void ConfigureRateLimitingOptions(this IServiceCollection services)
+    {
+        var rateLimitRules = new List<RateLimitRule>
+        {
+            new RateLimitRule
+            {
+                Endpoint = "*",
+                Limit = 3,
+                Period = "5m"
+            }
+        };
+
+        services.Configure<IpRateLimitOptions>(opt => { opt.GeneralRules = rateLimitRules; });
+        services.AddSingleton<IRateLimitCounterStore,MemoryCacheRateLimitCounterStore>();
+        services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+        services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+        services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+    }
 }
