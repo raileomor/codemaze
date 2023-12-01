0b2a0bfabd4bbf7839ed34ecf4d07474326b55a9
Rainer Leon <raileomor@gmail.com>
2023-12-01 12:27:35 -0300

    Creating resources and Model Binding

---
+++ Entities/Exceptions/IdParametersBadRequestException.cs
@@ -0,0 +1,9 @@
+namespace Entities.Exceptions;
+
+public sealed class IdParametersBadRequestException : BadRequestException
+{
+    public IdParametersBadRequestException()
+        :base("Parameter ids is null")
+    {
+    }
+}
