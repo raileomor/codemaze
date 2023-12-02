cddebac93b8c7ab881f4340bee67de94c9a9a68d
Rainer Leon <raileomor@gmail.com>
2023-12-02 13:10:17 -0300

    Working with PATCH requests

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -3,6 +3,8 @@ using CompanyEmployees.Extensions;
 using Contracts;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Options;
 using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -27,6 +29,7 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
 builder.Services.AddControllers(config => {
         config.RespectBrowserAcceptHeader = true;
         config.ReturnHttpNotAcceptable = true;
+        config.InputFormatters.Insert(0, GetJsonPatchInputFormatter());
     }).AddXmlDataContractSerializerFormatters()
     .AddCustomCSVFormatter()
     .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
@@ -55,3 +58,9 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+NewtonsoftJsonPatchInputFormatter GetJsonPatchInputFormatter() =>
+	new ServiceCollection().AddLogging().AddMvc().AddNewtonsoftJson()
+	.Services.BuildServiceProvider()
+	.GetRequiredService<IOptions<MvcOptions>>().Value.InputFormatters
+	.OfType<NewtonsoftJsonPatchInputFormatter>().First();
