328a10ea126579b53db72ae4b2e97a504555c2fe
Rainer Leon <raileomor@gmail.com>
2023-12-02 11:43:29 -0300

    Working with PUT requests

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -62,4 +62,15 @@ public class CompaniesController : ControllerBase
 
         return NoContent();
     }
+
+    [HttpPut("{id:guid}")]
+    public IActionResult UpdateCompany(Guid id, [FromBody] CompanyForUpdateDto company)
+    {
+        if (company is null)
+            return BadRequest("CompanyForUpdateDto object is null");
+
+        _service.CompanyService.UpdateCompany(id, company, trackChanges: true);
+
+        return NoContent();
+    }
 }
---
+++ Shared/DataTransferObjects/EmployeeForUpdateDto.cs
@@ -0,0 +1,5 @@
+namespace Shared.DataTransferObjects;
+
+public record EmployeeForUpdateDto(string Name, int Age, string Position);
+
+
