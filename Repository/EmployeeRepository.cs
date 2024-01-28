9e06b78d7754a20ff83dc6b11448913455673e19
Rainer Leon <raileomor@gmail.com>
2024-01-28 15:37:23 -0300

    Sorting

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -18,9 +18,7 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
         var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
             .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
             .Search(employeeParameters.SearchTerm)
-            .OrderBy(e => e.Name)
-            .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
-            .Take(employeeParameters.PageSize)
+            .Sort(employeeParameters.OrderBy)
             .ToListAsync();
 
         var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).CountAsync();
--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,4 +1,8 @@
+using System.Reflection;
+using System.Text;
 using Entities.Models;
+using System.Linq.Dynamic.Core;
+using Repository.Extensions.Utility;
 
 namespace Repository.Extensions;
 
@@ -16,5 +20,19 @@ public static class RepositoryEmployeeExtensions
         var lowerCaseTerm = searchTerm.Trim().ToLower();
 
         return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
+    }
+
+    public static IQueryable<Employee> Sort(this IQueryable<Employee> employees,
+        string? orderByQueryString)
+    {
+        if (string.IsNullOrWhiteSpace(orderByQueryString))
+            return employees.OrderBy(e => e.Name);
+
+        var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
+
+        if (string.IsNullOrWhiteSpace(orderQuery))
+            return employees.OrderBy(e => e.Name);
+
+        return employees.OrderBy(orderQuery);
     }
 }
